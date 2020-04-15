34686552a9f57f7c87d55be26065d31be7da33fd
Debayan <Debayan@DESKTOP-D931MGF>
2020-04-16 01:52:54 +0530

    Added Chart Component

---
+++ src/Components/Chart.js
@@ -0,0 +1,75 @@
+// import React, { Component } from 'react';
+// import { Chart } from "react-charts";
+
+// const chartObj =  {
+//   type: 'line',
+//   data: {
+//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//     type: 'line',
+//     datasets: [{
+//       data: [78, 81, 80, 45, 34, 12, 40],
+//       label: 'Dataset',
+//       backgroundColor: 'rgba(255,255,255,.1)',
+//       borderColor: 'rgba(255,255,255,.55)',
+//     },]
+//   },
+//   options: {
+//     maintainAspectRatio: true,
+//     legend: {
+//       display: false
+//     },
+//     layout: {
+//       padding: {
+//         left: 0,
+//         right: 0,
+//         top: 0,
+//         bottom: 0
+//       }
+//     },
+//     responsive: true,
+//     scales: {
+//       xAxes: [{
+//         gridLines: {
+//           color: 'transparent',
+//           zeroLineColor: 'transparent'
+//         },
+//         ticks: {
+//           fontSize: 2,
+//           fontColor: 'transparent'
+//         }
+//       }],
+//       yAxes: [{
+//         display: false,
+//         ticks: {
+//           display: false,
+//         }
+//       }]
+//     },
+//     title: {
+//       display: false,
+//     },
+//     elements: {
+//       line: {
+//         borderWidth: 0
+//       },
+//       point: {
+//         radius: 0,
+//         hitRadius: 10,
+//         hoverRadius: 4
+//       }
+//     }
+//   }
+// };
+
+// const ChartComponent = () => {
+//   return ( <Chart ></Chart> );
+// }
+
+// export default ChartComponent;
+
+//     //WidgetChart 1
+//     var ctx = document.getElementById("widgetChart1");
+//     if (ctx) {
+//       ctx.height = 130;
+
+// )
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import MoviesCount from "./MoviesCountComponent";
+import "../css/movieCount.css";
 
 class Main extends Component {
   state = {};
@@ -10,25 +12,9 @@ class Main extends Component {
           opacity: this.props.screenOpacity,
         }}
       >
-        <h2 className="mb-4">Sidebar #01</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="row m-t-25">
+          <MoviesCount></MoviesCount>
+        </div>
       </div>
     );
   }
